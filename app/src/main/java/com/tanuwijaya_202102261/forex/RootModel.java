45a2a3b35596bf742cda52032005151a75672a72
TANU WIJAYA <tanuxvi2@gmail.com>
2023-06-14 10:18:20 +0700

    Initial commit

---
+++ app/src/main/java/com/tanuwijaya_202102261/forex/RootModel.java
@@ -0,0 +1,16 @@
+package com.tanuwijaya_202102261.forex;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RootModel {
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+
+    public RootModel(){}
+
+    public  RatesModel getRatesModel() { return  ratesModel; }
+
+    public void setRatesModel(RatesModel ratesModel) {
+        this.ratesModel = ratesModel;
+    }
+}
